ae94594d975388b52decaec139f4dd7a3b8fb6f8
xiaoming-been <1294340611@qq.com>
2022-12-13 15:01:14 +0800

    update do_if.py

--- do_if.py
+++ do_if.py
@@ -20,3 +20,26 @@
 # else:
 #	print('严重肥胖')
 
+print('欢迎您使用BMI体测仪器')
+
+# []list列表 ()tuple元组
+
+p = ('过轻','正常','过重','肥胖','严重肥胖')
+height = float(input('请输入您的身高(m)：'))
+weight = float(input('请输入您的体重(kg)：'))
+
+BMI = weight/(height**2)
+print('您的BMI指数为：',BMI)
+
+if BMI<18.5:
+	print('您的评估结果是：',p[0])
+elif BMI>=18.5 and BMI<=25:
+	print('您的评估结果是：',p[1])
+elif BMI>=25 and BMI<=28:
+	print('您的评估结果是：',p[2])
+elif BMI>=28 and BMI<=32:
+	print('您的评估结果是：',p[3])
+elif BMI>32:
+	print('您的评估结果是：',p[4])
+
+print('欢迎您下次继续使用BMI体测仪器')
