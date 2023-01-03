2f32fe6764113553dbcd4f4002d15da3dc61b806
xiaoming-been <1294340611@qq.com>
2023-01-03 16:32:35 +0800

    update def_func.py

--- def_func.py
+++ def_func.py
@@ -24,3 +24,30 @@ def move(x,y,step,angle=0):
 
 x, y = move(100, 100, 60, math.pi / 6)
 print(x,y)
+
+# 定义函数时，需要确定函数名和参数个数
+# 如果有必要，可以先对参数的数据类型做检查；
+# 函数体内部可以用return随时返回函数结果；
+# 函数执行完毕也没有return语句时，自动return None。
+# 函数可以同时返回多个值，但其实就是一个tuple。
+
+# 定义一个函数quadratic(a, b, c)，接收3个参数，
+# 返回一元二次方程 ax^2+bx+c=0的两个解。
+# 计算平方根可以调用math.sqrt()函数
+import math
+def quadratic(a,b,c):
+	d=b*b-4*a*c
+	if a==0:
+		return("这不是二元一次方程")
+	elif d<0:
+		return("方程无解")
+	else:
+		x1=(-b+math.sqrt(d))/(2*a)
+		x2=(-b+math.sqrt(d))/(2*a)
+		return x1,x2
+	a=int(input("请输入a的值："))
+	b=int(input("请输入b的值："))
+	c=int(input("请输入c的值："))
+	x=quadratic(a,b,c)
+	print(x)
+print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
